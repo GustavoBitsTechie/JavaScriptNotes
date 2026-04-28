496f7ad8a17d9b281b62a48ba814f40b9c588222
GustavoBitsTechie <gc864053@gmail.com>
2026-04-28 10:31:55 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+//console.log("Hello World!");
+//console.log("This is a test website.");
+
+//window.alert("Welcome to my website!");
+//window.confirm("Do you like this website?");
+
+//This is just a test website to practice HTML, CSS, and JavaScript.//
+
+
+//document.getElementById("myH1").textContent = "Welcome!";
+
+//document.getElementById("myP").textContent = "This is a test website to practice HTML, CSS, and JavaScript.";
+
+/* let x = 5;
+let y = 10;
+let z = x + y;
+console.log(z);*/
+
+/*let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+console.log(price);
+console.log(age);
+console.log(gpa);
+console.log(`You are ${age} years old.`);
+console.log(`The price is $${price}.`);
+console.log(`Your gpa is ${gpa}.`);*/
+
+/*let firstName = "Gustavo";
+let lastName = "Cordova";
+let fullName = firstName + " " + lastName;
+let favoriteFood = "Pizza";
+let email = "WhatTheBalloon@gmail.com";
+
+console.log(firstName);
+console.log(lastName);
+console.log(favoriteFood);
+console.log(email);
+console.log(fullName);
+
+console.log (`My name is ${fullName} and I like ${favoriteFood}. You can contact me at ${email}.`);*/
+
+
+/*let online = true;
+let offline = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(`Bro is online: ${online}   Bro is offline: ${offline}`);
+console.log(`This item is for sale: ${forSale}`);
+console.log(`Bro is a student: ${isStudent}`);*/
+
+let fullName = ("Gustavo Cordova");
+let age = 25;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`;
+document.getElementById("p2").textContent = `You are ${age} years old.`;
+document.getElementById("p3").textContent = `It is ${isStudent} that you are a student.`;
